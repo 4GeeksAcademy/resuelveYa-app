2995110289e491bf0f93dfe008d3ddcdfce61918
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-11 22:16:24 +0000

    Component Navbar

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './styles/navbar.css'
 
 export const Navbar = () => {
 	return (
 		<nav className='bg-black text-white d-flex justify-content-between align-items-center p-2'>
-			<div><h1 className='mb-0 fw-bold'>Resuelve<span>Ya!</span></h1></div>
+			<div>
+				<Link to='/' className="text-white">
+					<h2 className='mb-0 fw-bold'>Resuelve<span>Ya!</span></h2>
+				</Link>
+			</div>
 
 			<form>
 				<input className='p-2 rounded' type="text" placeholder='Que estas buscando...?' />
 			</form>
 
-			<ul className='d-flex gap-3 my-auto fs-3 fw-semibold'>
+			<ul className='d-flex gap-3 my-auto fs-4 fw-semibold'>
 				<Link to='/login' className='text-white'>
-					<li className='list-group-item border-bottom-0'>Iniciar Sesion</li>
+					<li className='list-group-item'>Iniciar Sesion</li>
 				</Link>
-				<li className='list-group-item'>Registrate</li>
+				<Link to='/register' className="text-white">
+					<li className='list-group-item'>Registrate</li>
+				</Link>
 			</ul>
 
 		</nav>
