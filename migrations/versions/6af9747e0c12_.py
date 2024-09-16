934976c19a8bddef047e38d967327f4213d0925a
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-16 21:25:59 +0000

    nombre de usuario en el navbar

--- migrations/versions/6af9747e0c12_.py
+++ migrations/versions/6af9747e0c12_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 020834e073a1
+Revision ID: 6af9747e0c12
 Revises: 
-Create Date: 2024-09-16 21:22:24.872560
+Create Date: 2024-09-16 21:25:37.818133
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '020834e073a1'
+revision = '6af9747e0c12'
 down_revision = None
 branch_labels = None
 depends_on = None
