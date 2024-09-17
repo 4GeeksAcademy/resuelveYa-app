4407e4f9da420a1626e6295d687abb988670afb3
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-17 17:12:47 +0000

    conectando lista de servicios con back

--- src/front/js/component/CardService.jsx
+++ src/front/js/component/CardService.jsx
@@ -9,11 +9,16 @@ export const CardService = ({ user }) => {
                 <h4>Imagen</h4>
             </div>
             <div className='p-2'>
-                <h5>{user.username}</h5>
-                <p className='text-start'>Soy alguien que resuelve</p>
-                <p className='text-start border rounded p-2'><span className='fw-bold'>Horarios:</span>Lunes a viernes</p>
-                <div className='d-flex gap-2 justify-content-between align-items-center'>
-                    <p className='m-0 fw-bold'>{user.service_type}</p>
+                <h5 className='m-0'>{user.title}</h5>
+                <p className='text-center m-0 fs-5 fw-semibold text-secondary'>{user.username}</p>
+                <div className='border rounded p-2'>
+                    <p className='text-start m-0'><span className='fw-bold'>Descripcion: </span>{user.description}</p>
+                    <p className='text-start m-0'><span className='fw-bold'>Precio: </span>s/{user.price}</p>
+                    <p className='text-start m-0'><span className='fw-bold'>Horarios: </span>{user.service_time}</p>
+
+                </div>
+                <div className='d-flex mt-1 border rounded p-2 gap-2 justify-content-between align-items-center'>
+                    <p className='m-0 fw-bold' style={{textTransform: 'capitalize'}}>{user.service_type}</p>
                     <div className='d-flex gap-1 '>
                         <i className="fs-4 fa-solid fa-thumbs-up"></i>
                         <i className="fs-4 fa-solid fa-plus"></i>
