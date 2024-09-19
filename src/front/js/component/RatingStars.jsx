ea514f48eecf7fb62c22f4b383b941cc5c6016f3
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-09-19 22:52:58 +0000

    post list changes

---
+++ src/front/js/component/RatingStars.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const RatingStars = () => {
+
+    return(
+        <div className="d-flex flex-column align-items-center">
+        
+        </div>
+    );
+};
