c679b2cbe2987c2a9e2645b89c4596ed7d1af07f
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-09-25 01:54:40 +0000

    terms and conditions register finished

--- src/front/js/component/TermsConditionsRegister.jsx
+++ src/front/js/component/TermsConditionsRegister.jsx
@@ -5,11 +5,11 @@ const TermsConditionsRegister = ({ role }) => {
     <div>
       {role === "client" ? (
         <p>
-          Como cliente, debes tener cuidado con la información que ves en las publicaciones y no compartir datos personales o de contacto con terceros. Además, es importante ser respetuoso con los proveedores y cumplir con los acuerdos pactados.
+          Como cliente, debe tener cuidado con la información que reciba a través de las publicaciones y no compartir datos personales o de contacto con terceros. Además, es importante ser respetuoso con los proveedores y cumplir con los acuerdos pactados.
         </p>
       ) : (
         <p>
-          Como proveedor, debes pagar una tarifa plana desde S/30.00 por 1 mes para publicar tus servicios. Además, eres responsable de proporcionar información veraz y precisa sobre los servicios que ofreces. Asegúrate de cumplir con los acuerdos pactados y mantener una buena relación con los clientes.
+          Como proveedor, para poder realizar una publicación acerca de sus servicios a prestar, dede aceptar pagar una tarifa desde S/30.00 por 1 mes. Además, es responsable de proporcionar información veraz y precisa sobre los servicios que ofreces. Asegúrate de cumplir con los acuerdos pactados y mantener una buena relación con los clientes.
         </p>
       )}
     </div>
