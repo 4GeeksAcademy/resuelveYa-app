d3ce9ac8f93a3557b82c18ecee6e08f90e5c8df8
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-26 23:01:21 +0000

    estrellas de calificacion en las cards home

--- src/front/js/component/ListServices.jsx
+++ src/front/js/component/ListServices.jsx
@@ -5,16 +5,28 @@ import { Context } from "../store/appContext";
 export const ListServices = () => {
     const { actions, store } = useContext(Context)
     const [users, setUsers] = useState([])
-
+    console.log(store.reviews)
 
     return (
-        <div className='mt-4 w-100 list-services mx-auto py-5 w-md-75'>
+        <div className='mt-4 w-100 list-services mx-auto py-5'>
             <h1>Servicios más populares</h1>
-
-
             <div className="gallery js-flickity"
                 data-flickity-options='{ "wrapAround": true }'>
                 <div className="gallery-cell list-card-div list-card-1">
+                    <div className='bg-white d-flex justify-content-center align-items-center' style={{ height: '10%' }}>
+                        {
+                            [... new Array(5)].map((_, indx) => {
+                                const isRated = store.reviews[0]?.average_rating > indx;
+                                return isRated ?
+                                    <i className='bx bxs-star text-warning fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i> :
+                                    <i className='bx bx-star fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i>
+                            })
+                        }
+                    </div>
                     <div className="container-img-def">
                         <img src={store.reviews[0]?.post.post_img} alt="" />
                     </div>
@@ -23,6 +35,20 @@ export const ListServices = () => {
                     </h4>
                 </div>
                 <div className="gallery-cell list-card-div list-card-1">
+                    <div className='bg-white d-flex justify-content-center align-items-center' style={{ height: '10%' }}>
+                        {
+                            [... new Array(5)].map((_, indx) => {
+                                const isRated = store.reviews[1]?.average_rating > indx;
+                                return isRated ?
+                                    <i className='bx bxs-star text-warning fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i> :
+                                    <i className='bx bx-star fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i>
+                            })
+                        }
+                    </div>
                     <div className="container-img-def">
                         <img src={store.reviews[1]?.post.post_img} alt="" />
                     </div>
@@ -31,6 +57,20 @@ export const ListServices = () => {
                     </h4>
                 </div>
                 <div className="gallery-cell list-card-div list-card-1">
+                    <div className='bg-white d-flex justify-content-center align-items-center' style={{ height: '10%' }}>
+                        {
+                            [... new Array(5)].map((_, indx) => {
+                                const isRated = store.reviews[2]?.average_rating > indx;
+                                return isRated ?
+                                    <i className='bx bxs-star text-warning fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i> :
+                                    <i className='bx bx-star fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i>
+                            })
+                        }
+                    </div>
                     <div className="container-img-def">
                         <img src={store.reviews[2]?.post.post_img} alt="" />
                     </div>
@@ -39,6 +79,20 @@ export const ListServices = () => {
                     </h4>
                 </div>
                 <div className="gallery-cell list-card-div list-card-1">
+                    <div className='bg-white d-flex justify-content-center align-items-center' style={{ height: '10%' }}>
+                        {
+                            [... new Array(5)].map((_, indx) => {
+                                const isRated = store.reviews[3]?.average_rating > indx;
+                                return isRated ?
+                                    <i className='bx bxs-star text-warning fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i> :
+                                    <i className='bx bx-star fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i>
+                            })
+                        }
+                    </div>
                     <div className="container-img-def">
                         <img src={store.reviews[3]?.post.post_img} alt="" />
                     </div>
@@ -47,6 +101,20 @@ export const ListServices = () => {
                     </h4>
                 </div>
                 <div className="gallery-cell list-card-div list-card-1">
+                    <div className='bg-white d-flex justify-content-center align-items-center' style={{ height: '10%' }}>
+                        {
+                            [... new Array(5)].map((_, indx) => {
+                                const isRated = store.reviews[1]?.average_rating > indx;
+                                return isRated ?
+                                    <i className='bx bxs-star text-warning fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i> :
+                                    <i className='bx bx-star fa-lg d-flex justify-content-center align-items-center'
+                                        key={indx}>
+                                    </i>
+                            })
+                        }
+                    </div>
                     <div className="container-img-def">
                         <img src="https://img.freepik.com/free-photo/man-engaged-household-task_23-2151741266.jpg?t=st=1727368854~exp=1727372454~hmac=34ffaddc707b2a38186d6147318d594aab4ee5329ac6f4cea6b0af2bf8d8cb12&w=740" alt="" />
                     </div>
