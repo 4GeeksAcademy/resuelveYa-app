65eb367ac4f6bd8cf66721580ae48e3ba0dfaddf
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-09-26 19:43:47 +0000

    styles navbar

--- src/front/js/component/Jumbotron.jsx
+++ src/front/js/component/Jumbotron.jsx
@@ -1,39 +1,26 @@
 import React, { useState } from 'react';
 import "./styles/jumbotron.css"
-import bgImage from '../../../../public/bg.jpg'
+// import bgImage from '../../../../public/bg.jpg'
 import { SearchFilter } from './SearchFilter.jsx';
+import jardinero from '../../img/jardinero.jpg'
 
 export const Jumbotron = () => {
     const [searchInp1, setSearchInp1] = useState('')
     const [searchLocation, setSearchLocation] = useState('')
 
     return (
-        <header className="header text-white d-flex flex-column justify-content-center align-items-center">
-            {/* <form className=''>
-                <div className='d-flex'>
-                    <div className='bg-white w-100 mx-auto p-5 rounded container-form'>
-                        <input className='border rounded p-2' type='text' placeholder='Que estas buscando...?' />
-                        <input className='border rounded p-2' style={{maxHeight: '150px'}} type="text" placeholder='Donde...?' />
-                    </div>
-                </div>
-                <button className='btn btn-danger p-2 mt-1'>Buscar</button>
-            </form> */}
-            {/* <form className='w-75 p-4'>
-                <h1>Bienvenido a ResuelveYa!</h1>
-                <h2>Aqui encontraras la solucion</h2>
-                <div className='container-jumbotron'>
-                    <div className='input-1 input'>
-                        <i className="fa-solid fs-3 fa-magnifying-glass d-flex justify-content-center align-items-center"></i>
-                        <input className='' type="text" placeholder='¿Qué estás buscando...?' />
-                    </div>
-                    <div className='inpu-2 input'>
-                        <i className="fa-solid fs-3 fa-location-dot"></i>
-                        <input className='' style={{maxWidth: '5px'}} type='text' placeholder='¿Dónde...?' />
-                    </div>
-                </div>
-
-            </form> */}
+        <div className="header text-white d-flex flex-column justify-content-center align-items-center" style={{ paddingTop: "100px" }}>
             <SearchFilter title={'Bienvenido a ResuelveYa'} subTitle={'¡Encuentra tu solución aquí!'} />
-        </header>
+            <div>
+                <div>
+                    <img src={jardinero} alt="" />
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     )
 }
--- src/front/js/component/SearchFilter.jsx
+++ src/front/js/component/SearchFilter.jsx
@@ -5,8 +5,8 @@ import { Context } from '../store/appContext'
 import { useNavigate } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 
-export const SearchFilter = ({title, subTitle, posts}) => {
-    const {actions, store} = useContext(Context)
+export const SearchFilter = ({ title, subTitle, posts }) => {
+    const { actions, store } = useContext(Context)
     const navigate = useNavigate()
     const [postsData, setPostData] = useState([])
     const [searchTitleOrName, setSearchTitleOrName] = useState('')
@@ -15,14 +15,11 @@ export const SearchFilter = ({title, subTitle, posts}) => {
 
     const getPosts = async () => {
         const posts = await actions.getReviews()
-        // console.log(posts)
         setPostData(posts)
     }
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        // console.log(searchTitleOrName, location)
-        // actions.setListServices(filterByTitleAndName)
         navigate('/cardPostsList')
     }
 
@@ -30,52 +27,39 @@ export const SearchFilter = ({title, subTitle, posts}) => {
         setSearchTitleOrName(e.target.value)
         console.log(e.target.value)
     }
-    
+
     const filterByTitleAndName = postsData.filter((postProvider) => {
         const title = postProvider.post.title ? postProvider.post.title.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "") : "";
         const username = postProvider.post.user_name ? postProvider.post.user_name.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "") : "";
         const searchQuery = searchTitleOrName.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    
+
         return title.includes(searchQuery) || username.includes(searchQuery);
     });
 
     const filterByLocation = filterByTitleAndName.filter((postProvider => postProvider.post.location.toLowerCase().includes(locationProv.toLowerCase())))
 
-    // const filterByLocation = filterByTitleAndName.filter(postLocation => postLocation.location.toLowerCase().include(location))
-    
     useEffect(() => {
         actions.setReviews(filterByLocation)
     }, [searchTitleOrName, locationProv])
-    
-    // useEffect(() => {    
-    //     if (location.pathname === "/") {
-    //         console.log("hola desde home")
-
-    //         getPosts()
-    //     }
-      
-    // }, [location.pathname]);
 
     useEffect(() => {
         getPosts()
     }, [])
 
     return (
-        <form onSubmit={handleSubmit} className='form-filters-services w-75 mx-auto p-3 py-5 mb-5' style={{maxWidth: '700px'}}>
-            <h1 className='text-center text-white'>{title}</h1>
-            <h3 className='text-center text-white'>{subTitle}</h3>
-            <div className='container-jumbotron bg-white rounded p-2 container d-flex gap-2'>
+        <form onSubmit={handleSubmit} className='w-75 mx-auto p-3 py-5 mb-5' style={{ maxWidth: '700px' }}>
+            <h2 className='text-center text-color fw-bold'>{title}</h2>
+            <h4 className='text-center text-color'>{subTitle}</h4>
+            <div className='container-jumbotron bg-white rounded-5 p-2 container d-flex gap-2'>
                 <div className='d-flex align-items-center input-1'>
-                    <i className="fa-solid fs-3 fa-magnifying-glass"></i>
-                    <input className='' id='titleOrName' type="text" placeholder='¿Qué estás buscando?' onChange={handleChangeTitleAndName} autoComplete='off'/>
+                    <i className="fa-solid fs-4 fa-magnifying-glass pe-2" style={{ color: "rgb(208 201 201)" }}></i>
+                    <input className='' id='titleOrName' type="text" placeholder='¿Qué estás buscando?' onChange={handleChangeTitleAndName} autoComplete='off' />
                 </div>
                 <div className='d-flex align-items-center input-2'>
-                    <i className="fa-solid fs-3 fa-location-dot bg-white"></i>
-                    <input type="text" id='location' placeholder='¿Dónde?' onChange={(e) => setLocation(e.target.value)} maxLength={20} autoComplete='off'/>
+                    <i className="fa-solid fs-4 fa-location-dot bg-white pe-2" style={{ color: "rgb(208 201 201)" }} ></i>
+                    <input type="text" id='location' placeholder='¿Dónde?' onChange={(e) => setLocation(e.target.value)} maxLength={20} autoComplete='off' />
                 </div>
-            </div>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button className='button-search-filters p-2 fs-4 mt-3'>Buscar</button>
+                <button className='button-search-filters px-3 py-1 pb-2'>Buscar</button>
             </div>
         </form>
     )
