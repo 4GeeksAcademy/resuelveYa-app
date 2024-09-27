3e290d166c9af54dfdf762e1810fea034e168d41
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-09-27 15:41:43 +0000

    fixed styles home

--- migrations/versions/9e94276829dc_.py
+++ migrations/versions/9e94276829dc_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 15587949496a
+Revision ID: 9e94276829dc
 Revises: 
-Create Date: 2024-09-27 07:38:32.090318
+Create Date: 2024-09-27 14:26:32.182179
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '15587949496a'
+revision = '9e94276829dc'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/newPassword.js
+++ src/front/js/pages/newPassword.js
@@ -2,9 +2,9 @@ import React from "react";
 import { NewPasswordForm } from "../component/NewPasswordForm.jsx";
 
 export const NewPassword = () => {
-	return (
-        <div className="d-flex justify-content-center p-5">
+    return (
+        <div className="d-flex justify-content-center p-5" style={{ marginTop: "100px" }}>
             <NewPasswordForm />
         </div>
-	);
+    );
 };
